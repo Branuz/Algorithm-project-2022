61f2f9a88225a3bea516630570ca4ddac6d3e61e
Jyri P <jyri.poussu@gmail.com>
2022-11-12 17:40:53 +0200

    Added first version Dijikstras algorithm

---
+++ backend/src/main/java/com/helsinki/algorithm/pathFinders/dijkstras/Graph.java
@@ -0,0 +1,15 @@
+package com.helsinki.algorithm.pathFinders.dijkstras;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Graph {
+
+    private Set<Node> nodes = new HashSet<>();
+    
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+    // getters and setters 
+}
---
+++ backend/src/main/java/com/helsinki/algorithm/pathFinders/dijkstras/Node.java
@@ -0,0 +1,58 @@
+package com.helsinki.algorithm.pathFinders.dijkstras;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Node {
+    
+    private String name;
+    
+    private List<Node> shortestPath = new LinkedList<>();
+    
+    private Integer distance = Integer.MAX_VALUE;
+    
+    Map<Node, Integer> adjacentNodes = new HashMap<>();
+
+    public void addDestination(Node destination, int distance) {
+        adjacentNodes.put(destination, distance);
+    }
+ 
+    public Node(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setShortestPath(List<Node> shortestPath) {
+        this.shortestPath = shortestPath;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public void setAdjacentNodes(Map<Node, Integer> adjacentNodes) {
+        this.adjacentNodes = adjacentNodes;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Node> getShortestPath() {
+        return shortestPath;
+    }
+
+    public Integer getDistance() {
+        return this.distance;
+    }
+
+    public Map<Node, Integer> getAdjacentNodes() {
+        return this.adjacentNodes;
+    }
+
+}
